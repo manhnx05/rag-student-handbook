38e234cd8fa8cf5dec31bf63dbbd40b367120f72
Nguyễn Xuân Mạnh <nguyenxuanmanh918@gmail.com>
2026-07-07 23:44:29 +0700

    Remove medical schemas from schemas entry point

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,9 +1,1 @@
-
-from backend.app.schemas.patients import PatientBase, PatientCreate, Patient
-from backend.app.schemas.examinations import ExaminationBase, ExaminationCreate, Examination, ExaminationDetailBase, ExaminationDetailCreate, ExaminationDetail
-
-__all__ = [
-    "PatientBase", "PatientCreate", "Patient",
-    "ExaminationBase", "ExaminationCreate", "Examination",
-    "ExaminationDetailBase", "ExaminationDetailCreate", "ExaminationDetail"
-]
+# No relational schemas configured
