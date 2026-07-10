47f14329252e010d76c2307d930ed872f41d9280
Nguyễn Xuân Mạnh <nguyenxuanmanh918@gmail.com>
2026-07-10 22:14:17 +0700

    Initialize Shadcn UI and install required frontend dependencies

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,9 @@
 
 import './globals.css'
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata = {
   title: 'Student Handbook',
@@ -12,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="vi">
+    <html lang="vi" className={cn("font-sans", geist.variable)}>
       <body>{children}</body>
     </html>
   )
