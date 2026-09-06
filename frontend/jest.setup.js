d315dc0d757ea579353f5daa497916dbbf41610e
Nguyễn Xuân Mạnh <nguyenxuanmanh918@gmail.com>
2026-09-07 03:57:52 +0700

    Mock the syntax highlighter for Jest

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom'
+import React from 'react'
+
+jest.mock('react-syntax-highlighter', () => ({
+	Prism: ({ children }) => React.createElement('pre', null, children),
+}))
+
+jest.mock('react-syntax-highlighter/dist/esm/styles/prism', () => ({
+	vscDarkPlus: {},
+}))
